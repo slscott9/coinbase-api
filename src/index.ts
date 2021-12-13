80e1c6406e2851d09fefb16517957515915c8274
slscott9 <baseballdude9cf@gmail.com>
2021-12-13 14:02:55 -0600

    Fixing file structure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-import App from "app";
+import App from "./app";
+import 'dotenv/config';
+import 'module-alias/register';
 import CoinbaseController from "./resources/coinbase/coinbase.controller";
 import UserController from "./resources/user/user.controller";
 import UserRepository from "./resources/user/user.repository";
 import validateEnv from "./utils/validateEnv";
 
- 
-
  validateEnv();
 
  const userRepo: UserRepository = new UserRepository();
  userRepo.initialize();
+ 
  const app = new App(
      [ new CoinbaseController(userRepo), new UserController(userRepo)],
      Number(process.env.PORT)
